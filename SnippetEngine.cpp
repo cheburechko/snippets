e1aa17510abd7e2c99663c7857f43d5e1b69c964
Pavel Cherenkov <cherenkov.p.a@yandex.ru>
2017-05-20 23:20:05 +0700

    Added SnippetEngine

---
+++ SnippetEngine.cpp
@@ -0,0 +1,108 @@
+/*
+ * SnippetEngine.cpp
+ *
+ *  Created on: May 20, 2017
+ *      Author: thepunchy
+ */
+
+#include "SnippetEngine.h"
+#include <chrono>
+
+namespace snippets {
+
+SnippetEngine::SnippetEngine(SnippetStorage& snippets, TermDatabase& terms) :
+	snippets(snippets),
+	terms(terms),
+	bestPossibleScore(0),
+	bestScore(0)
+{
+}
+
+SnippetEngine::Result SnippetEngine::getSnippet(const std::string& query) {
+	using namespace std::chrono;
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+	SnippetEngine::Result result;
+
+	prepareQuery(query);
+	calculateBestPossibleScore();
+	while (bestScore < bestPossibleScore) {
+		rankNextSnippet();
+		selectNextBextTerm();
+	}
+
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	microseconds time_span = duration_cast<microseconds>(t2 - t1);
+	result.time = time_span.count();
+	result.snippet = bestSnippet;
+	return result;
+}
+
+void SnippetEngine::prepareQuery(const std::string& query) {
+	queryTerms = parser.getTermSet(query);
+	queryTermData.clear();
+	queryTermDataEnds.clear();
+
+	for (Term term : queryTerms) {
+		auto termData = terms.find(term);
+		if (termData == terms.end()) {
+			continue;
+		}
+		queryTermData.push_back(termData->second.begin());
+		queryTermDataEnds.push_back(termData->second.end());
+	}
+	bestScore = 0;
+	bestSnippet = "!!! Query had no matches. !!!";
+}
+
+void SnippetEngine::calculateBestPossibleScore() {
+	bestPossibleScore = 0;
+	termRank.clear();
+	for (unsigned i = 0; i < queryTermData.size(); i++) {
+		TermData::iterator& termData = queryTermData[i];
+		bestPossibleScore += termData->first;
+		termRank.emplace(termData->first, termData);
+	}
+}
+
+void SnippetEngine::rankNextSnippet() {
+	unsigned snippetID = queryTermData[termRank.begin()->second]->second;
+	if (analyzedSnippets.find(snippetID) != analyzedSnippets.end()) {
+		return;
+	}
+	Snippet& snippet = snippets.at(snippetID);
+	analyzedSnippets.emplace(snippetID);
+	double score = snippet.getTF().score(queryTerms);
+	if (score > bestScore) {
+		bestScore = score;
+		bestSnippet = snippet.getSnippet();
+	}
+}
+
+void SnippetEngine::selectNextBextTerm() {
+	unsigned termID = termRank.begin()->second;
+	auto& iter = queryTermData[termID];
+
+	termRank.erase(termRank.begin());
+	bestPossibleScore -= iter->first;
+	iter++;
+
+	while (
+		iter != queryTermDataEnds[termID] &&
+		analyzedSnippets.find(iter->second) != analyzedSnippets.end()
+	) {
+		iter++;
+	}
+	if (iter == queryTermDataEnds[termID]) {
+		return;
+	}
+
+	termRank.emplace(iter->first, termID);
+	bestPossibleScore += iter->first;
+}
+
+SnippetEngine::~SnippetEngine() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace snippets */
---
+++ SnippetEngine.h
@@ -0,0 +1,45 @@
+/*
+ * SnippetEngine.h
+ *
+ *  Created on: May 20, 2017
+ *      Author: thepunchy
+ */
+
+#ifndef SNIPPETENGINE_H_
+#define SNIPPETENGINE_H_
+
+#include "Parser.h"
+
+namespace snippets {
+
+class SnippetEngine {
+private:
+	SnippetStorage& snippets;
+	TermDatabase& terms;
+	Parser parser;
+
+	struct Result {
+		std::string snippet;
+		long time;
+	};
+
+	std::vector<TermData::iterator> queryTermData, queryTermDataEnds;
+	TermSet queryTerms;
+	std::unordered_set<unsigned> analyzedSnippets;
+	double bestScore, bestPossibleScore;
+	std::string bestSnippet;
+	std::multimap<double, unsigned, std::greater<double>> termRank;
+
+	void prepareQuery(const std::string& query);
+	void calculateBestPossibleScore();
+	void selectNextBextTerm();
+	void rankNextSnippet();
+public:
+	SnippetEngine(SnippetStorage& snippets, TermDatabase& terms);
+	Result getSnippet(const std::string& query);
+	virtual ~SnippetEngine();
+};
+
+} /* namespace snippets */
+
+#endif /* SNIPPETENGINE_H_ */
